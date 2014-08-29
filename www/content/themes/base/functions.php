626538bf191f3ed089915615fd5482404966e1f8
Andy <andrew.cocker@nameless.co.uk>
2014-08-29 07:28:43 -0700

    practise backbone app

--- www/content/themes/base/functions.php
+++ www/content/themes/base/functions.php
@@ -94,9 +94,13 @@ add_action( 'widgets_init', 'base_widgets_init' );
 function base_scripts() {
 	wp_enqueue_style( 'base-style', get_template_directory_uri() . '/css/main.css');
 
-	wp_enqueue_script( 'base-navigation', get_template_directory_uri() . '/js/main.min.js', array(), '20120206', true );
-
+	wp_enqueue_script( 'base-navigation', get_template_directory_uri() . '/js/main.js', array(), '20120206', true );
+  
+  //wp_enqueue_script( 'base-underscore', get_template_directory_uri() . '/js/libs/underscore-min.js', array(), '20140206', true );
+  wp_enqueue_script( 'base-build', get_template_directory_uri() . '/js/build.js', array(), '20140206', true );
 	wp_enqueue_script( 'base-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+  
+  wp_enqueue_script( 'base-back', get_template_directory_uri() . '/js/backbone.js', array(), '20120206', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
